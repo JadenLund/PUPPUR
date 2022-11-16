050609e7062edf458ec5a57e4ab74b6cbeb476dc
Jaden <lundjaden@gmail.com>
2022-11-16 14:31:35 -0500

    Adding image and fixing image import error

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Header from "semantic-ui-react/dist/commonjs/elements/Header/HeaderSubheader";
-import dogGroupPicture from "/home/jaden/code/labs/phase-5/PUPPUR/client/src/DogGroupPicture.png";
+// import dogGroupPicture from "/home/jaden/code/labs/phase-5/PUPPUR/client/src/DogGroupPicture.png";
 
 function About() {
   return (
@@ -9,7 +9,7 @@ function About() {
         This application was built to help you learn about different dog breeds
       </Header>
       <Header as="h3">Built by Jaden (Speckles) Lund</Header>
-      <img className="marquee" src={dogGroupPicture} />
+      <img className="marquee" src={"https://media2.giphy.com/media/326iF1qWfsTgKA4c78/giphy.gif?cid=6c09b952895bd3e79a8d60f61dc7362e095c919c2a692c76&rid=giphy.gif&ct=s"} />
     </div>
   );
 }
